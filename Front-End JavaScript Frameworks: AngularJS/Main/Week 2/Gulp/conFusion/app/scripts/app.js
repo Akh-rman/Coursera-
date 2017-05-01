42c51626a9abbf6b787610c4d72ca605738dcaee
Akh-rman <akh-rman@rambler.ru>
2017-05-01 16:07:53 +0300

    Сделал проверку формы с помощью Angular.

--- Front-End JavaScript Frameworks: AngularJS/Main/Week 2/Gulp/conFusion/app/scripts/app.js
+++ Front-End JavaScript Frameworks: AngularJS/Main/Week 2/Gulp/conFusion/app/scripts/app.js
@@ -42,8 +42,7 @@ app.controller('MenuController', ['$scope', function($scope) {
             comment: ''
         }
     ];
-
-            
+         
     $scope.select = function(setTab) {
         $scope.tab = setTab;
                 
@@ -68,8 +67,23 @@ app.controller('MenuController', ['$scope', function($scope) {
 
 app.controller('ContactController', ['$scope', function($scope) {
     $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+    $scope.channels = channels;
+    $scope.invalidChannelSelection = false;
 }])
 
 app.controller('FeedbackController', ['$scope', function($scope) {
-
+    $scope.sendFeedback = function() {
+        console.log($scope.feedback);
+        if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+            $scope.invalidChannelSelection = true;
+            console.log('incorrect');
+        } else {
+            $scope.invalidChannelSelection = false;
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            $scope.feedback.mychannel="";
+            $scope.feedbackForm.$setPristine();
+            console.log($scope.feedback);
+        }
+    };
 }]);
