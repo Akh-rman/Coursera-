b6ba30a088076b273a4bd8fd193b79bb387eb251
Akh-rman <akh-rman@rambler.ru>
2017-05-02 15:11:59 +0300

    Закончил выполнение задания с Angular Factory и Service.

--- Front-End JavaScript Frameworks: AngularJS/Main/Week 3/app/scripts/controllers.js
+++ Front-End JavaScript Frameworks: AngularJS/Main/Week 3/app/scripts/controllers.js
@@ -1,49 +1,12 @@
 'use strict';
 angular.module('confusionApp')
-.controller('MenuController', ['$scope', function($scope) {
+.controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
     $scope.tab = 1;
     $scope.filtText = '';
     $scope.showDetails = false;
-
-    $scope.dishes=[
-        {
-            name:'Uthapizza',
-            image: 'images/uthapizza.png',
-            category: 'mains', 
-            label:'Hot',
-            price:'4.99',
-            description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-            comment: ''
-        },
-        {
-            name:'Zucchipakoda', 
-            image: 'images/zucchipakoda.png',
-            category: 'appetizer', 
-            label:'',
-            price:'1.99',
-            description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-            comment: ''
-        },
-        {
-            name:'Vadonut', 
-            image: 'images/vadonut.png',
-            category: 'appetizer', 
-            label:'New',
-            price:'1.99',
-            description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-            comment: ''
-        },
-        {
-            name:'ElaiCheese Cake', 
-            image: 'images/elaicheesecake.png',
-            category: 'dessert', 
-            label:'',
-            price:'2.99',
-            description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-            comment: ''
-        }
-    ];
-                        
+    
+    $scope.dishes= menuFactory.getDishes();
+    
     $scope.select = function(setTab) {
         $scope.tab = setTab;
                 
@@ -92,7 +55,7 @@ angular.module('confusionApp')
         }
     };
 }])
-.controller('DishDetailController', ['$scope', function($scope) {
+.controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
     var dish={
         name:'Uthapizza',
         image: 'images/uthapizza.png',
@@ -134,7 +97,7 @@ angular.module('confusionApp')
         ]
     };
             
-    $scope.dish = dish;
+    $scope.dish = menuFactory.getDish(3);
 }])
 .controller('DishCommentController', ['$scope', function($scope) {           
     //Step 1: Create a JavaScript object to hold the comment from the form
@@ -151,5 +114,5 @@ angular.module('confusionApp')
         //Step 5: reset your JavaScript object that holds your comment
         $scope.review={author: "", rating: 5, comment: ""};
         //console.log($scope.review);
-    }
+    };
 }]);
