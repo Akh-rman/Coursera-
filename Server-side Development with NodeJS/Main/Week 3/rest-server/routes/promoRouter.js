000cf3ded1603c555ba2dd5873842cdc98c54859
Akh-rman <akh-rman@rambler.ru>
2017-08-20 20:11:15 +0300

    Добавил исправления в promoRouter.js.

--- Server-side Development with NodeJS/Main/Week 3/rest-server/routes/promoRouter.js
+++ Server-side Development with NodeJS/Main/Week 3/rest-server/routes/promoRouter.js
@@ -1,50 +1,63 @@
 var express = require('express'),
-    morgan = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+var Promotions = require('../models/promotions');
 
 var promoRouter = express.Router();
-
 promoRouter.use(bodyParser.json());
 promoRouter.route('/')
-    .all(function (req, res, next) {
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
+    .get(function(req, res, next) {
+        Promotions.find({}, function(err, promotion) {
+            if (err) throw err;
+            res.json(promotion);
         });
-        next();
     })
 
-    .get(function (req, res, next) {
-        res.end('Will send all the promotions to you!');
+    .post(function(req, res, next) {
+        Promotions.create(req.body, function(err, promotion) {
+            if (err) throw err;
+            console.log('Promotion created!');
+            var id = promotion._id;
+            
+            res.writeHead(200, {
+               'Content-Type': 'text/plain' 
+            });
+            res.end('Added the promotion with id: ' + id);
+        });
     })
 
-    .post(function (req, res, next) {
-        res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
-    })
-
-    .delete(function (req, res, next) {
-        res.end('Deleting all promotions');
+    .delete(function(req, res, next) {
+        Promotions.remove({}, function(err, resp) {
+            if (err) throw err;
+            res.json(resp);
+        });
     });
 
 promoRouter.route('/:promoId')
-    .all(function (req, res, next) {
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
+    .get(function(req, res, next) {
+        Promotions.findById(req.params.promotionId, function(err, promotion) {
+            if (err) throw err;
+            res.json(promotion);
         });
-        next();
     })
 
-    .get(function (req, res, next) {
-        res.end('Will send details of the promotion: ' + req.params.promoId + ' to you!');
+    .put(function(req, res, next) {
+        Promotions.findByIdAndUpdate(res.params.promitionId, {
+            $set: req.body
+        }, {
+           new: true 
+        }, function(err, promotion) {
+            if (err) throw err;
+            res.json(promotion);
+        });
     })
 
-    .put(function (req, res, next) {
-        res.write('Updating the promotion: ' + req.params.promoId + '\n');
-        res.end('Will update the promotion: ' + req.body.name +
-            ' with details: ' + req.body.description);
-    })
-
-    .delete(function (req, res, next) {
-        res.end('Deleting promotion: ' + req.params.promoId);
+    .delete(function(req, res, next) {
+        Promotions.findByIdAndRemove(req.params.promotionId, function(err, promotion) {
+            if (err) throw err;
+            res.json(resp);
+        })
     });
 
 module.exports = promoRouter;
