023351172e0c2b814a79b45faff98323f69505c6
Akh-rman <akh-rman@rambler.ru>
2018-04-30 20:03:03 +0300

    Basic Service.

--- Front-End JavaScript Frameworks: Angular/conFusion/src/app/app.module.ts
+++ Front-End JavaScript Frameworks: Angular/conFusion/src/app/app.module.ts
@@ -16,6 +16,8 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from "./services/dish.service";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -44,7 +46,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FlexLayoutModule,
     FormsModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
