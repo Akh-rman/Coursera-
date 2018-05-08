a3a5a9f7b3b09f0fc65e869cb7d691d9be2306ed
Akh-rman <akh-rman@rambler.ru>
2018-05-08 20:10:00 +0300

    Promise Part 1.

--- Front-End JavaScript Frameworks: Angular/conFusion/src/app/services/leader.service.ts
+++ Front-End JavaScript Frameworks: Angular/conFusion/src/app/services/leader.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Leader } from "../shared/leader";
 import { LEADERS } from "../shared/leaders";
+import {promise} from "selenium-webdriver";
 
 @Injectable()
 export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Leader[] {
-    return LEADERS;
+  getLeaders(): Promise<Leader[]> {
+    return Promise.resolve(LEADERS);
   }
 
-  getLeader(id: number): Leader {
-    return LEADERS.filter((leader) => (leader.id === id))[0];
+  getLeader(id: number): Promise<Leader> {
+    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
   }
 
-  getFeaturedLeader(): Leader {
-    return LEADERS.filter((leader) => leader.featured)[0];
+  getFeaturedLeader(): Promise<Leader> {
+    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
   }
 }
