d2f29309e253f7bf56051e6722fd1579dcc0c3dd
Akh-rman <akh-rman@rambler.ru>
2018-05-21 20:10:18 +0300

    Animations Part 1.

--- Front-End JavaScript Frameworks: Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Front-End JavaScript Frameworks: Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit, Inject} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 import { Params, ActivatedRoute } from "@angular/router";
 import { Location } from "@angular/common";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { trigger, state, style, animate, transition } from "@angular/animations";
 
 import { Dish } from "../shared/dish";
 import { DishService } from "../services/dish.service";
@@ -13,7 +14,20 @@ import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger("visibility", [
+      state("shown", style({
+        transform: "scale(1.0)",
+        opacity: 1
+      })),
+      state("hidden", style({
+        transform: "scale(0.5)",
+        opacity: 0
+      })),
+      transition("* => *", animate("0.5s ease-in-out"))
+    ])
+  ]
 })
 
 export class DishdetailComponent implements OnInit {
@@ -25,6 +39,7 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup;
   comment: Comment;
   errMess: string;
+  visibility = "shown";
 
   formErrors = {
     'author': '',
@@ -52,8 +67,8 @@ export class DishdetailComponent implements OnInit {
 
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-      .switchMap((params: Params) => this.dishService.getDish(+params["id"]))
-      .subscribe(dish => {this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id);},
+      .switchMap((params: Params) => { this.visibility = "hidden"; return this.dishService.getDish(+params["id"]) })
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = "shown"; },
         errmess => this.errMess = <any>errmess);
   }
 
@@ -82,7 +97,7 @@ export class DishdetailComponent implements OnInit {
     this.comment = this.commentForm.value;
     this.comment.date = new Date().toISOString();
     this.dishcopy.comments.push(this.comment);
-    this.dishcopy.save().subscribe(dish => {this.dish = dish; console.log(this.dish)});
+    this.dishcopy.save().subscribe(dish => { this.dish = dish; console.log(this.dish) });
     this.commentForm.reset({
       author: "",
       rating: 5,
